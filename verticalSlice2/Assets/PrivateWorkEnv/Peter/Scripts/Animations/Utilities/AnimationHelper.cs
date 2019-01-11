fdd7361977e6505a18951f66f3e1d61d1b7bd34b
PeterSchreuder_Laptop <24441@ma-web.nl>
2019-01-11 23:47:12 +0100

    Begin - Animatie placeholder werkt

---
+++ verticalSlice2/Assets/PrivateWorkEnv/Peter/Scripts/Animations/Utilities/AnimationHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class AnimationHelper : MonoBehaviour
+    {
+
+        [Range(0, 1)]
+        public float vertical;
+
+        Animator anim;
+
+        // Use this for initialization
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            anim.SetFloat("vertical", (float) vertical);
+        }
+    }
+}
+
