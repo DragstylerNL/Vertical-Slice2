66559ccb347fb08e465b79bd1122398ba9382738
DESKTOP-0BTH82I\Gebruiker <mjakobs11@hotmail.nl>
2019-01-12 12:39:39 +0100

    update

---
+++ verticalSlice2/Assets/PrivateWorkEnv/Martijn/Scripts/Controller/InputHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+
+    float vertical;
+    float horizontal;
+
+    StateManager states;
+	void Start ()
+    {
+        states = GetComponent<StateManager>();
+        states.Init();
+	}
+	
+
+	void FixedUpdate ()
+    {
+        GetInput();
+    }
+    void GetInput()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");   
+
+    }
+    void UpdateStates()
+    {
+      
+        states.Vertical = vertical;
+        states.Horizontal = horizontal;
+
+
+        states.Tick(Time.deltaTime);
+    }
+}
---
+++ verticalSlice2/Assets/PrivateWorkEnv/Martijn/Scripts/Controller/StateManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+
+    public float Horizontal;
+    public float Vertical;
+
+    public GameObject activeModel;
+    [HideInInspector]
+    public Animator anim;
+    [HideInInspector]
+    public Rigidbody rigid;
+    [HideInInspector]
+    public float delta;
+
+    public void Init()
+    {
+        SetupAnimator();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void SetupAnimator()
+    {
+
+        if (activeModel == null)
+        {
+            anim = activeModel.GetComponent<Animator>();
+            if (anim == null)
+            {
+                Debug.Log("no model found");
+            }
+            else
+            {
+                activeModel = anim.gameObject;
+            }
+        }
+
+        anim = GetComponentInChildren<Animator>();
+
+
+    }
+
+    public void Tick(float d)
+    {
+        delta = d;
+    }
+}
