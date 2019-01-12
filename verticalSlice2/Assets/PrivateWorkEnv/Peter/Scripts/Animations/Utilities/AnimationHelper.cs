7ae9d3614405f62f192bef973aa855254a2a7a07
PeterSchreuderMA <24441@ma-web.nl>
2019-01-12 21:57:45 +0100

    Finished - Animatie placeholders
    Ik zou nu alle gemaakte animaties van de Artists voor de speler in de Player object kunnen zetten.
    
    Ook zouden de placeholder models makkelijk veranderd kunnen worden.
    Zoals het wapen (Zie "WeaponHolderPlayer" in het placeholder model in Unity)
    
    Wat werkt:
     - Vloeiende overgang tussen animaties.
     - Root motion bij attack animaties.
     - 2 Makkelijk veranderbare floats voor het Horizontaal en Verticaale animatie blending.
    
    Wat zit er als placeholder in:
     - Walk / Jog
     - Strafe (Walk / Jog)
     - 3 one handed attacks
     - 2 two handed attacks
     - 1 use iten

--- verticalSlice2/Assets/PrivateWorkEnv/Peter/Scripts/Animations/Utilities/AnimationHelper.cs
+++ verticalSlice2/Assets/PrivateWorkEnv/Peter/Scripts/Animations/Utilities/AnimationHelper.cs
@@ -7,8 +7,23 @@ namespace SA
     public class AnimationHelper : MonoBehaviour
     {
 
-        [Range(0, 1)]
+        [Range(-1, 1)]
         public float vertical;
+
+        [Range(-1, 1)]
+        public float horizontal;
+
+        public bool playAnim;
+
+        public string[] oh_attacks;
+        public string[] th_attacks;
+
+        public bool twoHanded;
+        public bool enableRM; //Root motion
+        public bool useItem;
+        public bool interacting;
+        public float interactingSlow = 0.5f;
+        public bool lockon;
 
         Animator anim;
 
@@ -21,7 +36,70 @@ namespace SA
         // Update is called once per frame
         void Update()
         {
-            anim.SetFloat("vertical", (float) vertical);
+            //This is for the root motion applied when an attack ani ist running
+            enableRM = !anim.GetBool("canMove");
+            anim.applyRootMotion = enableRM;
+
+            //Check for the "interacting" bool
+            interacting = anim.GetBool("interacting");
+
+            if (lockon == false)
+            {
+                horizontal = 0;
+                vertical = Mathf.Clamp01(vertical);
+            }
+
+            anim.SetBool("lockon", lockon);
+
+            //Check if an attack ani is playing. If so, return
+            if (enableRM)
+                return;
+
+            if (useItem)
+            {
+                anim.Play("use_item");
+                useItem = false;
+            }
+
+            if (interacting)
+            {
+                playAnim = false;
+                vertical = Mathf.Clamp(vertical, 0, interactingSlow);
+            }
+
+            //Set the "twoHanded" to what ever it wants to be
+            anim.SetBool("two_handed", twoHanded);
+
+
+            //Debug: Play the animation
+            if (playAnim)
+            {
+                string targetAnim;
+
+                if (twoHanded)
+                {
+                    int r = Random.Range(0, th_attacks.Length);
+                    targetAnim = th_attacks[r];
+
+                    if (vertical > 0.5f)//When running and want to attack, play attack_3
+                        targetAnim = "oh_attack_3";
+                }
+                else
+                {
+                    int r = Random.Range(0, oh_attacks.Length);
+                    targetAnim = oh_attacks[r];
+                }
+
+                if (vertical > 0.5f)//When running and want to attack, play attack_3
+                    targetAnim = "oh_attack_3";
+
+                vertical = 0;
+                anim.CrossFade(targetAnim, 0.2f);
+                playAnim = false;
+            }
+
+            anim.SetFloat("vertical", vertical);
+            anim.SetFloat("horizontal", horizontal);
         }
     }
 }
