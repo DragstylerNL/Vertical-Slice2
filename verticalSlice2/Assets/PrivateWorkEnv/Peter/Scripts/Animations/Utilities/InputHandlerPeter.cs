a8020f0a431f88457ff872c9e8db07ae0794227c
PeterSchreuderMA <24441@ma-web.nl>
2019-01-13 11:47:38 +0100

    NB

---
+++ verticalSlice2/Assets/PrivateWorkEnv/Peter/Scripts/Animations/Utilities/InputHandlerPeter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandlerPeter : MonoBehaviour
+{
+    float vertical;
+    float horizontal;
+
+    bool b_input;
+    bool a_input;
+    bool x_input;
+    bool y_input;
+
+    bool rb_input;
+    float rt_input;
+    bool lb_input;
+    float lt_input;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
