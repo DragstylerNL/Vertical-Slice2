35de6636e0a2de952268732ace805990c2a477ac
PeterSchreuderMA <24441@ma-web.nl>
2019-01-13 13:41:08 +0100

    Begin - Player attacks

--- verticalSlice2/Assets/PrivateWorkEnv/Peter/Scripts/Animations/Utilities/InputHandlerPeter.cs
+++ verticalSlice2/Assets/PrivateWorkEnv/Peter/Scripts/Animations/Utilities/InputHandlerPeter.cs
@@ -13,9 +13,12 @@ public class InputHandlerPeter : MonoBehaviour
     bool y_input;
 
     bool rb_input;
-    float rt_input;
+    float rt_axes;
+    bool rt_input;
+
     bool lb_input;
-    float lt_input;
+    float lt_axes;
+    bool lt_input;
 
 
     // Use this for initialization
@@ -23,10 +26,29 @@ public class InputHandlerPeter : MonoBehaviour
     {
 		
 	}
+
+    void GetInput()
+    {
+        b_input = Input.GetButton("b_input");
+        rt_input = Input.GetButton("RT");
+        rt_axes = Input.GetAxis("RT");
+
+        if (rt_axes != 0)
+            rt_input = true;
+
+        lt_input = Input.GetButton("LT");
+        lt_axes = Input.GetAxis("LT");
+
+        if (lt_axes != 0)
+            lt_input = true;
+
+
+        Debug.Log(rt_input);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        
 	}
 }
