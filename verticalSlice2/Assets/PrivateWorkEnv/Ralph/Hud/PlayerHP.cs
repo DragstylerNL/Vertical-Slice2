73f6200b7d3a414d5625ad08602e12cd6dee592f
DragstylerNL <dragstylernl@gmail.com>
2019-01-14 11:16:44 +0100

    Health System Complete and boss animation
    Health System Complete and boss animation.
    also starting on text  and such

---
+++ verticalSlice2/Assets/PrivateWorkEnv/Ralph/Hud/PlayerHP.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : Health {
+    
+    // ==== Start
+	void Start () {
+        // boot  health
+        Bootup(1000,1000);
+
+	}
+
+}
