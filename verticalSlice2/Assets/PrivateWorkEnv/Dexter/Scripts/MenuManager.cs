8e273d51a35183e6bcfe3c2542bcd6825ccb19db
Dexter <itsdexter66@gmail.com>
2019-01-28 08:44:55 +0100

    Begin aan startmenu gemaakt

---
+++ verticalSlice2/Assets/PrivateWorkEnv/Dexter/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the scene.
+    /// </summary>
+    /// <param name="CanvasScene">Scene name.</param>
+    public void LoadScene(string CanvasScene)
+    {
+        SceneManager.LoadScene(CanvasScene);
+    }
+
+
+    /// <summary>
+    /// Exits the game.
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
