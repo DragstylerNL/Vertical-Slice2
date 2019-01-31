550baca483eb627346b91a0f09aee185a23c1591
Dexter <itsdexter66@gmail.com>
2019-01-31 14:10:36 +0100

    Started a SceneLoader

---
+++ verticalSlice2/Assets/PrivateWorkEnv/Dexter/Scripts/SceneLoadManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour
+{
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log(progress);
+
+            yield return null;
+        }
+
+    }
+
+    /// <summary>
+    /// Exits the game.
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
